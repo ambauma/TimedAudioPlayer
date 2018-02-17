cc0a29636f25658398b14ff05b9e3840d476cf19
ambauma <andrew.m.baumann@gmail.com>
2018-02-17 10:25:53 -0600

    First working version

---
+++ src/test/java/io/github/ambauma/audioplayer/MainTest.java
@@ -0,0 +1,15 @@
+package io.github.ambauma.audioplayer;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class MainTest {
+
+
+
+    @Test
+    public void testFindFilePath() throws Exception {
+        //assertEquals("", AudioPlayer.findFilePath(""))
+    }
+}
