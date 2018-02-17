e96a895fd311563a00c7aecea3545bc590539ff2
ambauma <andrew.m.baumann@gmail.com>
2018-02-17 14:29:04 -0600

    added several plugins, especially checkstyle, and fixed checkstyle warnings

--- src/main/java/io/github/ambauma/audioplayer/Main.java
+++ src/main/java/io/github/ambauma/audioplayer/Main.java
@@ -1,20 +1,15 @@
 package io.github.ambauma.audioplayer;
 
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.FileSystems;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.StandardOpenOption;
-import java.util.*;
-
 public class Main {
 
-    public static void main(String[] args) throws Exception {
-        final String fileName = args[0];
-        final int duration = Integer.parseInt(args[1]);
-
-
-        final AudioPlayer audioPlayer = new AudioPlayer(fileName, duration);
-    }
+  /**
+   * Main entrypoint of the application.
+   * @param args  The args to process.  Expects a folder path and a duration in milliseconds
+   * @throws Exception  possible exception
+   */
+  public static void main(String[] args) throws Exception {
+    final String folderName = args[0];
+    final int duration = Integer.parseInt(args[1]);
+    new AudioPlayer(folderName, duration);
+  }
 }
